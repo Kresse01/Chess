8d2b82ce5e7b646a67bd65db8025f75b2f6893bb
Gustav Kres <gustav.kres@gmail.com>
2025-10-25 17:24:03 +0200

    Tog bort build mappen

--- build/CMakeFiles/ch_bb_smoke.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ch_bb_smoke.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/run_bb_smoke.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for run_bb_smoke.
